01e9a6ea1d204081a7b2f4dbcb862394319debe2
bhargav1251 <bhargav4java@gmail.com>
2018-02-25 00:57:26 -0500

    intial commit1

---
+++ firstmodule/src/main/java/com/example/firstmodule/FirstmoduleApplication.java
@@ -0,0 +1,12 @@
+package com.example.firstmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstmoduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstmoduleApplication.class, args);
+	}
+}
---
+++ firstmodule/src/test/java/com/example/firstmodule/FirstmoduleApplicationTests.java
@@ -0,0 +1,19 @@
+package com.example.firstmodule;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FirstmoduleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	  String str = "Bhargav";
+    Assert.assertTrue(str.length()>0);
+	}
+
+}
